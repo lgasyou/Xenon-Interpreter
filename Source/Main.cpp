054f90e20fd3f2f66f6e9b74ac511f6484ff183c
李泽清 <lgasyou@163.com>
2017-08-30 15:10:43 +0800

    First commit.

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -1,13 +1,22 @@
 #include "Stable.h"
 #include "Utils/FileReader.h"
 
+class Script;
+
 int main(int argc, char *argv[]) {
-	if (argc != 1) {
-		char *fileName = argv[1];
-		FileReader reader{ fileName };
-		std::string source =  reader.readAll();
-		DBG_PRINT << source;
+	if (argc == 1) {
+		return 0;
 	}
+
+	char *fileName = argv[1];
+	FileReader reader{ fileName };
+	std::string source = reader.readAll();
+
+	Script script = Script::Compile(source);
+	std::string result = script.Run();
+	std::cout << result;
+
+	DBG_PRINT << source;
 	system("pause");
     return 0;
 }
